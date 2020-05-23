d4197768ac6c94820269688327196106fc823be9
eliasallex <cod3er4@gmail.com>
2020-05-23 17:18:37 -0300

    Feat: auto reload

---
+++ main.js
@@ -0,0 +1,4 @@
+"use strict";
+
+require("./src");
+require("electron-reload")(__dirname);
---
+++ src/index.js
@@ -0,0 +1,46 @@
+const { app, BrowserWindow, globalShortcut } = require("electron");
+const path = require("path");
+
+let win = null;
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 1300,
+    height: 700,
+    titleBarStyle: "hidden",
+    title: "Loading..",
+    alwaysOnTop: false,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  const file = path.join(__dirname, "public", "index.html");
+  win.loadFile(file);
+}
+
+function toggleDevTools() {
+  win.webContents.toggleDevTools();
+}
+
+function createShortcuts() {
+  globalShortcut.register("CmdOrCtrl+j", toggleDevTools);
+  globalShortcut.register("AltOrOption", () => {
+    console.log("Alt action");
+    win.toggleTabBar();
+  });
+}
+
+app.whenReady().then(createWindow).then(createShortcuts);
+
+app.on("window-all-closed", () => {
+  if (process.platform === "darwin") {
+    return app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
