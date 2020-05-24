dd72f9e36a959f467242576334cc84314ea1fa66
eliasallex <cod3er4@gmail.com>
2020-05-24 19:53:25 -0300

    Feat: webview struture

--- src/public/script/aside.js
+++ src/public/script/aside.js
@@ -1,6 +1,6 @@
-const {
-  ipcMain
-} = require('electron')
+// const {
+//   ipcMain
+// } = require('electron')
 
 // ipcMain.on('explorer', (event, value) => {
 //   console.log(value)
@@ -9,17 +9,88 @@ const {
 const btnClose = document.querySelector('#btn-close')
 const searchElement = document.querySelector('button#get')
 const searchInput = document.querySelector('input#search-input')
+const prevHistory = document.querySelector('button#prev-history')
+const nextHistory = document.querySelector('button#next-history')
+
+prevHistory.addEventListener('click', () => {
+  window.history.back()
+})
+nextHistory.addEventListener('click', () => {
+  window.history.forward()
+})
 
 const getWiewContent = () => document.querySelector('div#content-group')
 
+// fecha a tab View
 btnClose.addEventListener('click', () => {
   console.log('Fechando!')
+  clear_history()
   getWiewContent().classList.remove('window-actived')
 })
 
+// destroy o elemento web atual
+const destroyWebview = () => {
+  const parent = document.querySelector('div#webview webview')
+  if (parent) {
+    parent.remove()
+  }
+}
+
+// cria um webview e add ao seu parent
+const createWebview = (url) => {
+  destroyWebview()
+
+  const parent = document.querySelector('div#webview')
+
+  let element = document.createElement('webview')
+  element.src = `https://www.${url}`;
+  element.setAttribute('id', 'content')
+
+  parent.appendChild(element)
+}
+
+// Pesquisa as urls
 searchElement.addEventListener('click', (event) => {
   if (searchInput.value) {
     const link = searchInput.value
-    console.log(link)
+
+    createWebview(link)
+    loadWeview()
   }
 })
+
+const loadWeview = () => {
+  try {
+    const title = document.querySelector('label#title')
+    const webview = document.querySelector('webview')
+    // const indicator = document.querySelector('.indicator')
+
+    const loadstart = () => {
+      title.innerHTML = '.......'
+      // indicator.style.display = 'block'
+      // indicator.innerText = 'loading...'
+    }
+
+    const loadstop = () => {
+      const endereco = (webview.src) ? webview.src : 'https://'
+      console.log(endereco)
+      const getEndereco = () => endereco.replace('https://', '')
+
+      title.innerHTML = getEndereco()
+      // indicator.innerText = ''
+      // indicator.style.display = 'none'
+    }
+
+    webview.addEventListener('did-start-loading', loadstart)
+    webview.addEventListener('did-stop-loading', loadstop)
+  } catch (err) {
+    console.warn(err)
+  }
+}
+
+const clear_history = () => {
+  console.log('------------- History -------------')
+  window.document.cookie = null
+  window.localStorage = null
+  window.history = null
+}
